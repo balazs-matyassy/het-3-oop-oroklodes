b46ee9c8b713ac489b5c10115d8a1e2d19246316
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-04-28 17:26:47 +0200

    mb: update

---
+++ src/hu/progmatic/lesson_20220428_01_store/Store.java
@@ -0,0 +1,17 @@
+package hu.progmatic.lesson_20220428_01_store;
+
+public class Store {
+    private final Inventory[] inventories;
+
+    public Store(int inventorySize) {
+        this.inventories = new Inventory[inventorySize];
+    }
+
+    public void addInventory(Inventory inventory) {
+
+    }
+
+    public void printInventories() {
+
+    }
+}
